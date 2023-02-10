40ba044c33ff06164f4cb811963826d160598a1f
Hanna Oberlander <122308635+jalapenobagelgirl@users.noreply.github.com>
2023-02-10 14:38:38 -0800

    j

--- music-genre-hanna/script.js
+++ music-genre-hanna/script.js
@@ -2,4 +2,10 @@
 $('.artist1').click(function() {
     $('.artist1-content').toggleClass('show')
 })
+$('.artist2').click(function() {
+    $('.artist2-content').toggleClass('show')
+})
+$('.artist3').click(function() {
+    $('.artist3-content').toggleClass('show')
+})
 
