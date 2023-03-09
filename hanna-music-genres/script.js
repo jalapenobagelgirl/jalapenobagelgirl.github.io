a53ee53912137f5aa330200324a88aa063a43647
Hanna Oberlander <122308635+jalapenobagelgirl@users.noreply.github.com>
2023-03-08 18:19:55 -0800

    work wednesday 3/8

--- hanna-music-genres/script.js
+++ hanna-music-genres/script.js
@@ -1,15 +1,11 @@
 
-
-$('.artist1').hover(function() {
+$('.artist1').click(function() {
     $('.artist1-content').toggleClass('show')
 })
-$('.artist2').hover(function() {
+$('.artist2').click(function() {
     $('.artist2-content').toggleClass('show')
 })
-$('.artist3').hover(function() {
+$('.artist3').click(function() {
     $('.artist3-content').toggleClass('show')
-})
-$('.artist4').hover(function() {
-    $('.artist4-content').toggleClass('show')
 })
 
