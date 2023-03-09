5117c3dbcf36073c9c449a73fe1c636dabbd10a4
Hanna Oberlander <122308635+jalapenobagelgirl@users.noreply.github.com>
2023-03-08 18:36:51 -0800

    fwefwfe

--- hanna-music-genres/script.js
+++ hanna-music-genres/script.js
@@ -1,4 +1,36 @@
 
+$('.jbalvin').click(function() {
+    $('.jbalvin-content').toggleClass('show')
+})
+$('.artist2').click(function() {
+    $('.artist2-content').toggleClass('show')
+})
+$('.artist3').click(function() {
+    $('.artist3-content').toggleClass('show')
+})
+
+$('.artist1').click(function() {
+    $('.artist1-content').toggleClass('show')
+})
+$('.artist2').click(function() {
+    $('.artist2-content').toggleClass('show')
+})
+$('.artist3').click(function() {
+    $('.artist3-content').toggleClass('show')
+})
+
+
+$('.artist1').click(function() {
+    $('.artist1-content').toggleClass('show')
+})
+$('.artist2').click(function() {
+    $('.artist2-content').toggleClass('show')
+})
+$('.artist3').click(function() {
+    $('.artist3-content').toggleClass('show')
+})
+
+
 $('.artist1').click(function() {
     $('.artist1-content').toggleClass('show')
 })
