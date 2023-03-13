c737e54063687069dfe5bfe855b7b7bb269fb864
Hanna Oberlander <122308635+jalapenobagelgirl@users.noreply.github.com>
2023-03-12 21:49:51 -0700

    sdv

--- hanna-music-genres/script.js
+++ hanna-music-genres/script.js
@@ -1,43 +1,5 @@
-
-$('.jbalvin').click(function() {
-    $('.jbalvin-content').toggleClass('show')
-})
-$('.daddyyankee').click(function() {
-    $('.daddyyankee-content').toggleClass('show')
-})
-$('.ozuna').click(function() {
-    $('.ozuna-content').toggleClass('show')
-})
-
-$('.supremes').click(function() {
-    $('.supremes-content').toggleClass('show')
-})
-$('.beegees').click(function() {
-    $('.beegees-content').toggleClass('show')
-})
-$('.louis').click(function() {
-    $('.louis-content').toggleClass('show')
-})
-
-
-$('.avril').click(function() {
-    $('.avril-content').toggleClass('show')
-})
-$('.britney').click(function() {
-    $('.britney-content').toggleClass('show')
-})
-$('.jason').click(function() {
-    $('.jason-content').toggleClass('show')
-})
-
-
-$('.backkeys').click(function() {
-    $('.blackkeys-content').toggleClass('show')
-})
-$('.cage').click(function() {
-    $('.cage-content').toggleClass('show')
-})
-$('.ziggy').click(function() {
-    $('.ziggy-content').toggleClass('show')
-})
-
+$( function() {
+    $( ".jbalvin" ).click( function() {
+      $( this ).toggleClass( ".content" );
+    });
+  });
