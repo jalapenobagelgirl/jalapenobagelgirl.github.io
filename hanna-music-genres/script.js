957169f1cc0dff216310d9512a622be4389e8e23
Hanna Oberlander <122308635+jalapenobagelgirl@users.noreply.github.com>
2023-03-22 15:43:45 -0700

    push it

--- hanna-music-genres/script.js
+++ hanna-music-genres/script.js
@@ -2,13 +2,3 @@ $('.disc').hover(function () {
   $(this).find('.content').toggleClass('hide')
 })
 
-
-var vid = document.getElementById("myVideo");
-
-function getVolume() { 
-  alert(vid.volume);
-} 
-  
-function setHalfVolume() { 
-  vid.volume = 0.2;
-} 
